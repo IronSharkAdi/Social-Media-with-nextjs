4f221d0a62b59c1c05b19fa5a7597cd847cb46fe
IronSharkAdi <ironsharkadi2020@gmail.com>
2021-12-20 00:17:54 +0600

    Header Icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+// import moduleName from '@'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
